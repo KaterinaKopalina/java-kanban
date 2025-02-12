ef3dc3be2bb945aa4c95da07faaae30f5470f3aa
Katerina Kopalina <bonefan@yandex.ru>
2025-02-12 15:36:16 +0300

    Исправлен конструктор Убран метод set. Добавлен метод deleteSubtask

--- src/Epic.java
+++ src/Epic.java
@@ -1,16 +1,12 @@
 import java.util.ArrayList;
 public class Epic extends Task {
 
+
         private ArrayList<Subtask> subtaskList = new ArrayList<>();
 
-       public Epic(String name, String description) {
+        public Epic(String name, String description) {
             super(name, description);
         }
-
-        public Epic(int id, String name, String description, Status status) {
-            super(id, name, description, status);
-        }
-
         public void addSubtask(Subtask subtask) {
             subtaskList.add(subtask);
         }
@@ -23,18 +19,21 @@ public class Epic extends Task {
             return subtaskList;
         }
 
-        public void setSubtaskList(ArrayList<Subtask> subtaskList) {
-            this.subtaskList = subtaskList;
+        public void deleteSubtask(Subtask subtask) {
+            subtaskList.remove(subtask);
         }
 
-        @Override
-        public String toString() {
-            return "Epic{" +  " id=" + getId() +
-                    " name= " + getName() + '\'' +
-                    ", description = " + getDescription() + '\'' +
-                    ", subtaskList.size = " + subtaskList.size() +
-                    ", status = " + getStatus() +
-                    '}';
-        }
+
+    public String toString() {
+        return "Epic{"
+                +"id="+getId()
+                +", name=\""+getName()
+                +"\", description=\""+getDescription()
+                +",\nstatus="+getStatus()
+                +"}";
     }
+}
+
+
+
 
